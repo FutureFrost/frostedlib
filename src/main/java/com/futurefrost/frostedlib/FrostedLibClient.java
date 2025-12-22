1b4525a82a94114df6321018fa8c4f3c199589c0
Future Frost <jay.melville2006@gmail.com>
2025-12-22 16:24:47 -0700

    Imported and Configured Fabric Template Mod
    I have no idea what I am doing, keeping this as a fallback state.

---
+++ src/main/java/com/futurefrost/frostedlib/FrostedLibClient.java
@@ -0,0 +1,10 @@
+package com.futurefrost.frostedlib;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FrostedLibClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
